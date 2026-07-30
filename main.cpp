297880d5594494129d34ff3c155630515a2afe86
Nipun G <nipun.gadapa@gmail.com>
2026-07-30 13:24:57 +0530

    Directly creating objects inside the array

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-using namespace std;
+
 /*Class Definitions*/
 class System;
 class Student;
@@ -11,20 +11,20 @@ class Student
 private:
     int rank;
     bool isAssigned = false;
-    string AssignedSeatCode = "0000";
-    string *choiceList;
+    std::string AssignedSeatCode = "0000";
+    std::string *choiceList;
 
 public:
     friend class System;
-    Student(int rnk, string list[]);
+    Student(int rnk, std::string list[]);
     void display()
     {
-        cout << "Student Rank: " << rank << endl;
-        cout << "Student Assigned Seat: " << AssignedSeatCode << endl;
+        std::cout << "Student Rank: " << rank << std::endl;
+        std::cout << "Student Assigned Seat: " << AssignedSeatCode << std::endl;
     }
 };
 
-Student::Student(int rnk, string list[])
+Student::Student(int rnk, std::string list[])
 {
     rank = rnk;
     choiceList = list;
@@ -33,20 +33,20 @@ Student::Student(int rnk, string list[])
 class Seats
 {
 private:
-    string seatCode;
+    std::string seatCode;
     int availableSeats;
 
 public:
     friend class System;
-    Seats(string code, int count);
+    Seats(std::string code, int count);
     void display()
     {
-        cout << "Seat Code: " << seatCode << endl;
-        cout << "Seats Available: " << availableSeats << endl;
+        std::cout << "Seat Code: " << seatCode << std::endl;
+        std::cout << "Seats Available: " << availableSeats << std::endl;
     }
 };
 
-Seats::Seats(string code, int count)
+Seats::Seats(std::string code, int count)
 {
     seatCode = code;
     availableSeats = count;
@@ -66,52 +66,51 @@ public:
     {
         for (int i = 0; i < seatsListSize; i++)
         {
-            cout << "Seat Code: " << seatsList[i].seatCode << endl;
-            cout << "Available Seats: " << seatsList[i].availableSeats << endl;
+            std::cout << "Seat Code: " << seatsList[i].seatCode << std::endl;
+            std::cout << "Available Seats: " << seatsList[i].availableSeats << std::endl;
         }
     }
     void displayStudents()
     {
         for (int i = 0; i < studentsListSize; i++)
         {
-            cout << "Student Rank: " << studentsList[i].rank << endl;
-            cout << "Assigned Seat Code: " << studentsList[i].AssignedSeatCode << endl;
-            cout<<"-----------------"<<endl;
+            std::cout << "Student Rank: " << studentsList[i].rank << std::endl;
+            std::cout << "Assigned Seat Code: " << studentsList[i].AssignedSeatCode << std::endl;
+            std::cout << "-----------------" << std::endl;
         }
     }
 
     void AssignSeats()
     {
-        for (int i = 0; i < 15; i++)
-        {   
-            //cout<<"Entered Student Loop"<<endl;
-            for (int j = 0; j < 3; j++)
-            {  
-                //cout<<"Entered Choices Loop"<<endl;
-                // if studentsList[i].choiceList[j] in seatsList and its count > 0, then assign.
-                for (int k = 0; k < 3; k++)
-                {   //cout<<"Entered Assignment Loop"<<endl;
+        for (int i = 0; i < studentsListSize; i++)
+        {
+            // cout<<"Entered Student Loop"<<endl;
+            for (int j = 0; j < seatsListSize; j++)
+            {
+                // cout<<"Entered Choices Loop"<<endl;
+                //  if studentsList[i].choiceList[j] in seatsList and its count > 0, then assign.
+                for (int k = 0; k < seatsListSize; k++)
+                { // cout<<"Entered Assignment Loop"<<endl;
                     if (studentsList[i].choiceList[j] == seatsList[k].seatCode && seatsList[k].availableSeats > 0)
                     {
                         studentsList[i].AssignedSeatCode = seatsList[k].seatCode;
                         studentsList[i].isAssigned = true;
-                         seatsList[k].availableSeats = seatsList[k].availableSeats - 1;
-                         //cout<<"Assigned Successfully"<<endl;
+                        seatsList[k].availableSeats = seatsList[k].availableSeats - 1;
+                        // cout<<"Assigned Successfully"<<endl;
                         goto exit_choices;
                     }
                     else
                     {
-                        //cout<<"Assignment Failed, iterating"<<endl;
+                        // cout<<"Assignment Failed, iterating"<<endl;
                         continue;
                     }
-                    
                 }
-                //cout<<"Exited Assignment Loop"<<endl;
+                // cout<<"Exited Assignment Loop"<<endl;
             }
-            exit_choices:
-            //cout<<"Exited Choices Loop"<<endl;
+        exit_choices:
+            std::cout << "Exited Choices Loop" << std::endl;
         }
-        //cout<<"Exited Student Lopp"<<endl;
+        // cout<<"Exited Student Lopp"<<endl;
     }
 };
 
@@ -125,31 +124,29 @@ System::System(Seats array1[], int seatListSize, Student array2[], int studentLi
 
 int main()
 {
-    string c1[] = {"0101", "0102", "0103"};
-    string c2[] = {"0102", "0101", "0103"};
-    string c3[] = {"0101", "0103", "0102"};
-    Student s1(1, c1);
-    Student s2(2, c2);
-    Student s3(3, c3);
-    Student s4(4, c1);
-    Student s5(5, c2);
-    Student s6(6, c3);
-    Student s7(7, c1);
-    Student s8(8, c2);
-    Student s9(9, c3);
-    Student s10(10, c1);
-    Student s11(11, c2);
-    Student s12(12, c3);
-    Student s13(13, c1);
-    Student s14(14, c2);
-    Student s15(15, c3);
+    std::string c1[] = {"0101", "0102", "0103"};
+    std::string c2[] = {"0102", "0101", "0103"};
+    std::string c3[] = {"0101", "0103", "0102"};
 
-    Seats seat1("0101", 5);
-    Seats seat2("0102", 5);
-    Seats seat3("0103", 5);
+    Student array1[] = {Student(1, c1),
+                        Student(2, c2),
+                        Student(3, c3),
+                        Student(4, c1),
+                        Student(5, c2),
+                        Student(6, c3),
+                        Student(7, c1),
+                        Student(8, c2),
+                        Student(9, c3),
+                        Student(10, c1),
+                        Student(11, c2),
+                        Student(12, c3),
+                        Student(13, c1),
+                        Student(14, c2),
+                        Student(15, c3)};
 
-    Student array1[] = {s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15};
-    Seats array2[] = {seat1, seat2, seat3};
+    Seats array2[] = {Seats("0101", 5),
+                      Seats("0102", 5),
+                      Seats("0103", 5)};
 
     System sys(array2, 3, array1, 15);
     sys.AssignSeats();
