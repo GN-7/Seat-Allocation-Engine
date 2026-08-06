ea9bc394fecfeb75994eb497b7ec98efd4a894e7
Nipun G <nipun.gadapa@gmail.com>
2026-08-06 18:59:00 +0530

    CSV READER

--- csv_reader.cpp
+++ csv_reader.cpp
@@ -1,23 +1,39 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-int main()
-{
+void createStudentObject(){
     std::fstream file("file.csv");
     std::string line;
-
     std::getline(file, line);
+
+
     while (std::getline(file, line))
     {
 
         std::string name = line.substr(0, line.find(','));
-        std::string rank = line.substr(line.find(',') + 1, line.length());
+        line = line.substr(line.find(',') + 1, line.length());
 
-        // std:: string rank = line.substr(0, line.find(','));
-        // std:: string line = line.substr(line.find(',') + 1, line.length());
+        std::string rank = line.substr(0, line.find(','));
+        line = line.substr(line.find(',') + 1, line.length());
 
-        std::cout << name << std::endl;
-        std::cout << rank << std::endl;
-    }
+        std::string allotedSeat = line.substr(0, line.find(','));
+        line = line.substr(line.find(',') + 1, line.length());
+
+        std::string choice1 = line.substr(0, line.find(','));
+        line = line.substr(line.find(',') + 1, line.length());
+
+        std::string choice2 = line.substr(0, line.find(','));
+        line = line.substr(line.find(',') + 1, line.length());
+
+        std::string choice3 = line.substr(0, line.find(','));
+        line = line.substr(line.find(',') + 1, line.length());
+
+        std::string choiceList[] = {choice1, choice2, choice3};
+    };
+}
+
+
+int main()
+{
     return 0;
 }
