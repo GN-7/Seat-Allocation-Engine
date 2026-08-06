4daeb16ab7ba60b4c3420879efd0e7be7d804441
Nipun G <nipun.gadapa@gmail.com>
2026-08-06 19:37:22 +0530

    Updates

--- csv_reader.cpp
+++ csv_reader.cpp
@@ -1,39 +1,64 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-void createStudentObject(){
-    std::fstream file("file.csv");
-    std::string line;
-    std::getline(file, line);
 
+class AllocationEngine;
 
-    while (std::getline(file, line))
-    {
+class Student{
+    friend class AllocationEngine;
+    int rank;
+    std::string allotedSeat;
+    std::string *choiceList;
 
-        std::string name = line.substr(0, line.find(','));
-        line = line.substr(line.find(',') + 1, line.length());
-
-        std::string rank = line.substr(0, line.find(','));
-        line = line.substr(line.find(',') + 1, line.length());
-
-        std::string allotedSeat = line.substr(0, line.find(','));
-        line = line.substr(line.find(',') + 1, line.length());
-
-        std::string choice1 = line.substr(0, line.find(','));
-        line = line.substr(line.find(',') + 1, line.length());
-
-        std::string choice2 = line.substr(0, line.find(','));
-        line = line.substr(line.find(',') + 1, line.length());
-
-        std::string choice3 = line.substr(0, line.find(','));
-        line = line.substr(line.find(',') + 1, line.length());
-
-        std::string choiceList[] = {choice1, choice2, choice3};
-    };
+    Student::Student(int r, std::string list[3]);
+};
+Student::Student(int r, std::string list[3]){
+    rank = r;
+    choiceList = list;
 }
 
 
+class AllocationEngine
+{
+private:
+
+public:
+    void createStudentObjects()
+    {
+
+        std::fstream file("file.csv");
+        std::string line;
+        std::getline(file, line);
+
+        while (std::getline(file, line))
+        {
+
+            std::string name = line.substr(0, line.find(','));
+            line = line.substr(line.find(',') + 1, line.length());
+
+            std::string rank = line.substr(0, line.find(','));
+            line = line.substr(line.find(',') + 1, line.length());
+
+            std::string allotedSeat = line.substr(0, line.find(','));
+            line = line.substr(line.find(',') + 1, line.length());
+
+            std::string choice1 = line.substr(0, line.find(','));
+            line = line.substr(line.find(',') + 1, line.length());
+
+            std::string choice2 = line.substr(0, line.find(','));
+            line = line.substr(line.find(',') + 1, line.length());
+
+            std::string choice3 = line.substr(0, line.find(','));
+            line = line.substr(line.find(',') + 1, line.length());
+
+            std :: string choiceList[] = {choice1, choice2, choice3};
+        };
+    }
+};
+
 int main()
 {
+    AllocationEngine sys;
+    sys.createStudentObjects();
     return 0;
 }
